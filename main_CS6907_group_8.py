27101dcc1384147bfc1ee981b8a58e44fd4fa27e
AmCh-Q <AmCh-Q@users.noreply.github.com>
2023-12-11 10:17:59 -0500

    Add files via upload

--- main_CS6907_group_8.py
+++ main_CS6907_group_8.py
@@ -1,5 +1,4 @@
 # Configuring task, and dataset
-# Modified from https://github.com/ardkastrati/EEGEyeNet
 from config import config, build_file_name
 config['task'] = 'Position_task'
 config['dataset'] = 'dots'
@@ -7,27 +6,27 @@ config['preprocessing'] = 'min' # or max
 config['feature_extraction'] = True # must be set to True for ML_models operating on feature extracted data
 config['include_ML_models'] = True
 config['include_DL_models'] = True
-config['include_your_models'] = False
+config['include_your_models'] = True
 config['all_EEG_file'] = build_file_name()
 
 # Add our model and SOTA
 import benchmark
 from hyperparameters import merge_models
-from our_models import Ours_pretrained, EEGViT_pretrained
+from our_models import Ours_Pretrained
 additional_models = {
     'Position_task': {
         'dots' : {
             'min_temp' : {
-                'EEGViT': EEGViT_pretrained,
-                'Ours': Ours_pretrained
+                #'EEGViT': EEGViT_pretrained,
+                'Ours': Ours_Pretrained
             }
         }
     }
 }
-benchmark.all_models = merge_models(benchmark.all_models, additional_models)
+benchmark.all_models = merge_models(main.benchmark.all_models, additional_models)
 
 # Training and benchmarking the models
-# Modified from https://github.com/ardkastrati/EEGEyeNet
 import main
 main.config = config
+main.benchmark.all_models = benchmark.all_models
 main.main()
--- our_models.py
+++ our_models.py
@@ -1,7 +1,9 @@
+import torch
 from torch import nn
+import numpy as np
 import transformers
+import logging
 from DL_Models.torch_models.torch_utils.dataloader import create_dataloader
-from DL_Models.torch_models.torch_utils.training import test_loop
 
 class EEGViT_pretrained(nn.Module):
     def __init__(self):
@@ -23,34 +25,61 @@ class EEGViT_pretrained(nn.Module):
         filename = path + self.model_name + '.sav'
         self.model = pickle.load(open(filename, 'rb'))
 
-class Ours_pretrained(nn.Module):
-    def __init__(self):
+class Ours_Pretrained(nn.Module):
+    def __init__(self, nb_models = 5, batch_size = 64, n_epoch = 15, learning_rate = 1e-4, vit_model_name = "google/vit-base-patch16-224"):
         super().__init__()
-        self.model_name = "Ours_pretrained"
-        self.vit_model_name = "google/vit-base-patch16-224"
+        self.model_name = "Ours_Pretrained"
+        self.nb_models = nb_models
+        self.models = []
+
+        self.batch_size = batch_size
+        self.n_epoch = n_epoch
+        self.learning_rate = learning_rate
+        self.vit_model_name = vit_model_name
+
         self.conv1 = nn.Conv2d(
             in_channels=1,
             out_channels=256,
             kernel_size=(1,16),
             stride=(1,16),
             padding=(0,6),
-            bias=False
-        )
+            bias=False)
         self.batchnorm1 = nn.BatchNorm2d(256, False)
-        config = transformers.ViTConfig.from_pretrained(self.vit_model_name)
+        self.model_configs = transformers.ViTConfig.from_pretrained(self.vit_model_name)
         config.update({'num_channels': 256})
         config.update({'image_size': (129,32)})
         config.update({'patch_size': (129,1)})
-        self.model = transformers.ViTForImageClassification.from_pretrained(
-            self.vit_model_name, config=config, ignore_mismatched_sizes=True)
-        self.model.vit.embeddings.patch_embeddings.projection = nn.Conv2d(
+        if torch.cuda.is_available():
+            gpu_id = 0    # Change this to the desired GPU ID if you have multiple GPUs
+            torch.cuda.set_device(gpu_id)
+            self.device = torch.device(f"cuda:{gpu_id}")
+        else:
+            self.device = torch.device("cpu")
+
+    def forward(self,x):
+        x=self.conv1(x)
+        x=self.batchnorm1(x)
+        x=self.ViT.forward(x).logits
+        return x
+
+    def create_model(self):
+        model = transformers.ViTForImageClassification.from_pretrained(
+            self.vit_model_name, config=self.model_configs, ignore_mismatched_sizes=True)
+        model.vit.embeddings.patch_embeddings.projection = nn.Conv2d(
             256, 768,
             kernel_size=(129,1), stride=(129,1), padding=(0,0),
             groups=256)
-        self.model.classifier = nn.Sequential(
+        model.classifier = nn.Sequential(
             nn.Linear(768,1000,bias=True),
             nn.Dropout(p=0.1),
             nn.Linear(1000,2,bias=True))
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=6, gamma=0.1)
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        model = model.to(self.device)
+        self.criterion = nn.MSELoss().to(self.device)
+        return model
 
     def fit(self, trainX, trainY, validX, validY):
         # Create dataloaders
@@ -62,20 +91,88 @@ class Ours_pretrained(nn.Module):
         for i in range(self.nb_models):
             logging.info("------------------------------------------------------------------------------------")
             logging.info('Start fitting model number {}/{} ...'.format(i+1, self.nb_models))
-            model = self.model(loss = self.loss, model_number=i, batch_size=self.batch_size, **self.model_params)
-            model.fit(train_dataloader, validation_dataloader)
+            model = self.create_model()
+            for epoch in range(n_epoch):
+                logging.info("-------------------------------")
+                logging.info(f"Epoch {epoch+1}")
+                # Train the model
+                model.train()
+                epoch_train_loss = 0.0
+                for i, (index, inputs, targets) in tqdm(enumerate(train_dataloader)):
+                    # Move the inputs and targets to the GPU (if available)
+                    inputs = inputs.to(self.device)
+                    targets = targets.to(self.device)
+                    # Compute the outputs and loss for the current batch
+                    self.optimizer.zero_grad()
+                    outputs = model(inputs)
+                    loss = criterion(outputs.squeeze(), targets.squeeze())
+                    # Compute the gradients and update the parameters
+                    loss.backward()
+                    self.optimizer.step()
+                    epoch_train_loss += loss.item()
+                    # Print the loss and accuracy for the current batch
+                    if i in printBatchNumList:
+                        print(f"Epoch {epoch+1}, Batch {i}, Training Loss: {loss.item()}", end='')
+                epoch_train_loss /= len(train_dataloader)
+                logging.info(f"Avg training loss: {epoch_train_loss:>7f}")
+                print(f"Epoch {epoch+1}, Training Loss: {epoch_train_loss}", end='')
+                # Evaluate the model
+                model.eval()
+                with torch.no_grad():
+                    val_loss = 0.0
+                    for index, inputs, targets in enumerate(validation_dataloader):
+                        # Move the inputs and targets to the GPU (if available)
+                        inputs = inputs.to(self.device)
+                        targets = targets.to(self.device)
+                        # Compute the outputs and loss for the current batch
+                        outputs = model(inputs)
+                        loss = criterion(outputs.squeeze(), targets.squeeze())
+                        val_loss += loss.item()
+                    val_loss /= len(validation_dataloader)
+                logging.info(f"Avg validation loss: {val_loss:>8f}")
+                print(f"Epoch {epoch+1}, Val Loss: {val_loss}")
             self.models.append(model)
             logging.info('Finished fitting model number {}/{} ...'.format(i+1, self.nb_models))
 
     def predict(self, testX):
-        pass
+        testX = np.transpose(testX, (0, 2, 1))  # (batch_size, samples, channels) to (bs, ch, samples) as torch conv layers want it
+        a,b,c = testX.shape
+        a = self.batch_size - a % self.batch_size
+        dummy = np.zeros((a,b,c))
+        testX = np.concatenate((testX, dummy)) # TO ADD batch_size - testX.shape[0]%batch_size
+        test_dataloader = create_dataloader(testX, testX, self.batch_size, self.model_name, drop_last=False)
+        pred = None
+        for model in self.models:
+            for index, inputs, _ in enumerate(test_dataloader):
+                # Move the inputs to the GPU (if available)
+                inputs = inputs.to(self.device)
+                # Compute the outputs
+                outputs = model(inputs)
+                if batch == 0:
+                    all_pred = outputs.cpu()
+                else:
+                    all_pred = torch.cat((all_pred, outputs.cpu()))
+            if pred is not None:
+                pred += all_pred.data.numpy()
+            else:
+                pred = all_pred.data.numpy()
+        pred = pred[:-a]
+        return pred / len(self.models)
 
     def save(self, path):
-        import pickle
-        filename = path + self.model_name + '.sav'
-        pickle.dump(self.model, open(filename, 'wb'))
+        for i, model in enumerate(self.models):
+            ckpt_dir = path + self.model_name + '_nb_{}_'.format(i) + '.pth'
+            torch.save(model.state_dict(), ckpt_dir)
 
     def load(self, path):
-        import pickle
-        filename = path + self.model_name + '.sav'
-        self.model = pickle.load(open(filename, 'rb'))
+        self.models = []
+        for file in os.listdir(path):
+            if not self.load_file_pattern.match(file):
+                continue
+            # These 2 lines are needed for torch to load
+            logging.info(f"Loading model nb from file {file} and predict with it")
+            model = self.create_model()  # model = TheModelClass(*args, **kwargs)
+            #print(path + file)
+            model.load_state_dict(torch.load(path + file))  # model.load_state_dict(torch.load(PATH))
+            model.eval()  # needed before prediction
+            self.models.append(model)
